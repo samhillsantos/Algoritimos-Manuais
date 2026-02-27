b2e9f7df41fde7afcd1f408447482483cbb92733
samhillsantos <seu-email@exemplo.com>
2026-02-27 18:49:52 -0300

    Estruturando pastas

---
+++ Java/02_Busca/Binaria.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Binaria{
+	public static int vetor[] = new int[11];
+	public static void gerarVetor(){
+		System.out.print("[");
+		for(int i = 1; i < vetor.length; i++){
+			vetor[i] = i;
+			System.out.print(vetor[i] + (i < vetor.length - 1 ? ", " : ""));
+		}
+		System.out.print("]");
+	}
+
+	public static int buscaBinaria(){
+		return -1;
+	}
+	public static void main(String[] agrs){
+		gerarVetor();
+	}
+}
