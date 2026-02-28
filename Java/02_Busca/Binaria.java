ac218d9a12e46ab8909c2670aa43dd96abcd1308
samhillsantos <seu-email@exemplo.com>
2026-02-27 23:58:09 -0300

    Ciração de um vetor de idades

--- Java/02_Busca/Binaria.java
+++ Java/02_Busca/Binaria.java
@@ -3,7 +3,12 @@ import java.util.Arrays;
 
 public class Binaria{
 	public static int vetor[] = new int[11];
-	public static void gerarVetor(){
+
+    public static void main(String[] args){
+
+    }
+	
+    public static void gerarVetor(){
 		System.out.print("[");
 		for(int i = 1; i < vetor.length; i++){
 			vetor[i] = i;
@@ -13,9 +18,7 @@ public class Binaria{
 	}
 
 	public static int buscaBinaria(){
+
 		return -1;
-	}
-	public static void main(String[] agrs){
-		gerarVetor();
 	}
 }
---
+++ Java/04_Matematica/Vetores.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Vetores{
+    // Declarações globais
+    public static Scanner input = new Scanner(System.in);
+    public static int[] idades;
+    public static final int TAM_MIN = 1;
+    public static final int TAM_MAX = 30;
+
+    public static void main(String[] args) {
+        int qtdPessoas;
+        
+        // Verificação de digitação
+        do {
+            System.out.printf("Digite a quantidade de pessoas Limite %d até %d: ",TAM_MIN, TAM_MAX); 
+            qtdPessoas = input.nextInt();
+        } while (qtdPessoas < TAM_MIN || qtdPessoas > TAM_MAX); 
+        
+        idades = new int[qtdPessoas]; 
+
+        // Input de idades
+        for(int i = 0; i < qtdPessoas; i+= 1){
+            System.out.println("Digite as idades: ");
+            idades[i] = input.nextInt();
+        }
+        // Impressão de inputs
+        System.out.printf("[");
+        for(int i = 0; i < qtdPessoas; i++){
+            System.out.print(idades[i]);
+            if(i < qtdPessoas -1){
+                System.out.print(", ");
+            }
+        }
+        System.out.printf("]");
+    }
+}
